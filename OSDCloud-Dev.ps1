a54f2ea94f76d3ba31e6e21e3b77b982954603ca
Sam <mainsails@users.noreply.github.com>
2023-09-04 15:27:34 +0100

    Update OSDCloud-Dev.ps1

--- OSDCloud-Dev.ps1
+++ OSDCloud-Dev.ps1
@@ -1,1 +1,39 @@
-Start-OSDCloud
+#=======================================================================
+#   OS: Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSBuild = "22H2"
+    OSEdition = "Pro"
+    OSLanguage = "en-us"
+    OSLicense = "Retail"
+    SkipAutopilot = $true
+    SkipODT = $true
+}
+Start-OSDCloud @Params
+
+#=======================================================================
+#   PostOS: AutopilotOOBE Staging
+#=======================================================================
+$AutopilotOOBEJson = @'
+{
+    "Assign":  {
+                   "IsPresent":  true
+               },
+    "GroupTag":  "Enterprise",
+    "GroupTagOptions":  [
+                            "Development",
+                            "Enterprise"
+                        ],
+    "Hidden":  [
+                   "AddToGroup",
+                   "AssignedComputerName",
+                   "AssignedUser",
+                   "PostAction"
+               ],
+    "PostAction":  "Quit",
+    "Run":  "NetworkingWireless",
+    "Docs":  "https://autopilotoobe.osdeploy.com/",
+    "Title":  "OSDeploy Autopilot Registration"
+}
+'@
+$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.JSON -Encoding ascii -Force
