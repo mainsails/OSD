143ea6dc2b2bf87ec1e50a91790cdb97151dc0f8
Sam <mainsails@users.noreply.github.com>
2023-09-04 15:35:47 +0100

    Update OSDCloud-Dev.ps1

--- OSDCloud-Dev.ps1
+++ OSDCloud-Dev.ps1
@@ -1,1 +1,12 @@
-Start-OSDCloud
+#=======================================================================
+#   OS: Params and Start-OSDCloud
+#=======================================================================
+$Params = @{
+    OSBuild = "22H2"
+    OSEdition = "Pro"
+    OSLanguage = "en-us"
+    OSLicense = "Retail"
+    SkipAutopilot = $true
+    SkipODT = $true
+}
+Start-OSDCloud @Params
