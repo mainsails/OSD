7b98f86d990b09f465cf8f7aab0b2c0adcfcee45
Sam <mainsails@users.noreply.github.com>
2023-09-05 17:00:38 +0100

    Update OSDCloud-OOBE.ps1

--- OSDCloud-OOBE.ps1
+++ OSDCloud-OOBE.ps1
@@ -1,10 +1,5 @@
 Start-Transcript -Path "c:\test.txt"
 
-$scriptFolderPath = "$env:SystemDrive\OSDCloud\testing"
-If(!(Test-Path -Path $scriptFolderPath)) {
-    New-Item -Path $scriptFolderPath -ItemType Directory -Force | Out-Null
-}
-
 Install-Module AutopilotOOBE -Force -Verbose
 Start-AutopilotOOBE -Verbose
 
