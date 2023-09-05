e527d036dd206ebc3fe5b1f5227cdc213323ba34
Sam <mainsails@users.noreply.github.com>
2023-09-05 16:37:05 +0100

    Update OSDCloud-Dev.ps1

--- OSDCloud-Dev.ps1
+++ OSDCloud-Dev.ps1
@@ -1,63 +1,24 @@
-#================================================
-#  [PreOS] Params and Start-OSDCloud
-#================================================
+# Start OSDCloud
 Write-Host -ForegroundColor Green "Start OSDCloud"
 $Params = @{
-    OSVersion = "Windows 11"
-    OSBuild = "22H2"
-    OSEdition = "Pro"
+    OSVersion  = "Windows 11"
+    OSBuild    = "22H2"
+    OSEdition  = "Pro"
     OSLanguage = "en-gb"
-    OSLicense = "Retail"
-    ZTI = $true
-    Firmware = $false
+    OSLicense  = "Retail"
+    ZTI        = $true
+    Firmware   = $false
 }
 Start-OSDCloud @Params
 
-#================================================
-#  [PostOS] AutopilotOOBE Configuration Staging
-#================================================
-Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
-$AutopilotOOBEJson = @"
-{
-    "AssignedComputerName" : "$AssignedComputerName",
-    "AddToGroup":  "AADGroupX",
-    "Assign":  {
-                   "IsPresent":  true
-               },
-    "GroupTag":  "GroupTagXXX",
-    "Hidden":  [
-                   "AddToGroup",
-                   "AssignedUser",
-                   "PostAction",
-                   "GroupTag",
-                   "Assign"
-               ],
-    "PostAction":  "Quit",
-    "Run":  "NetworkingWireless",
-    "Docs":  "https://google.com/",
-    "Title":  "Autopilot Manual Register"
-}
-"@
-
-If (!(Test-Path "C:\ProgramData\OSDeploy")) {
-    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
-}
-$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
-
-#================================================
-#  [PostOS] SetupComplete CMD Command Line
-#================================================
+# Create C:\Windows\Setup\Scripts\SetupComplete.cmd (runs in OOBE)
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
-set LOCALAPPDATA=%USERPROFILE%AppDataLocal
-powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
-powershell.exe -Command "& {IEX (IRM https://raw.githubusercontent.com/mainsails/OSD/main/OSDCloud-OOBE.ps1)}"
+powershell.exe -ExecutionPolicy Bypass -Command "& {IEX (IRM https://raw.githubusercontent.com/mainsails/OSD/main/OSDCloud-OOBE.ps1)}"
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
-#=======================================================================
-#   Restart-Computer
-#=======================================================================
-Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+# Restart Computer in 5 seconds
+Write-Host  -ForegroundColor Green "Restart computer in 5 seconds"
 Start-Sleep -Seconds 20
 wpeutil reboot
