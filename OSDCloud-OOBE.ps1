b05a0339d93c97071cb1581adfd47fab20ab589e
Sam <mainsails@users.noreply.github.com>
2023-09-05 14:30:59 +0100

    Update OSDCloud-OOBE.ps1

--- OSDCloud-OOBE.ps1
+++ OSDCloud-OOBE.ps1
@@ -3,8 +3,12 @@ If(!(Test-Path -Path $scriptFolderPath)) {
     New-Item -Path $scriptFolderPath -ItemType Directory -Force | Out-Null
 }
 
+Start-Transcript -Path $scriptFolderPath
+
 Write-Host -ForegroundColor DarkGray "Installing AutopilotOOBE PS Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Install-Module AutopilotOOBE -Force -Verbose" -Wait
 
 Write-Host -ForegroundColor DarkGray "Executing AutopilotOOBE Module"
 Start-Process PowerShell -ArgumentList "-NoL -C Start-AutopilotOOBE" -Wait
+
+Stop-Transcript
