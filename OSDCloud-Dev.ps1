5801c948998da746b7ffc71bf23f8f962bb4b885
Sam <mainsails@users.noreply.github.com>
2023-09-05 10:24:34 +0100

    Update OSDCloud-Dev.ps1

--- OSDCloud-Dev.ps1
+++ OSDCloud-Dev.ps1
@@ -1,48 +1,94 @@
-#=======================================================================
-#   OS: Params and Start-OSDCloud
-#=======================================================================
+#================================================
+#  [PreOS] Params and Start-OSDCloud
+#================================================
 $Params = @{
-    OSBuild = "22H2"
-    OSEdition = "Pro"
-    OSLanguage = "en-us"
-    OSLicense = "Retail"
-    SkipAutopilot = $true
-    SkipODT = $true
+    OSName       = 'Windows 11 22H2 x64'
+    OSLanguage   = 'en-gb'
+    OSEdition    = 'Pro'
+    OSActivation = 'Retail'
+    OSBuild      = "22H2"
+    ZTI          = $true
+    Firmware     = $false
 }
 Start-OSDCloud @Params
 
-
-#=======================================================================
-#   PostOS: AutopilotOOBE Staging
-#=======================================================================
-$AutopilotOOBEJson = @'
+#================================================
+#  [PostOS] OOBEDeploy Configuration
+#================================================
+Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json"
+$OOBEDeployJson = @'
 {
+    "AddNetFX3":  {
+                      "IsPresent":  true
+                  },
+    "Autopilot":  {
+                      "IsPresent":  false
+                  },
+    "UpdateDrivers":  {
+                          "IsPresent":  true
+                      },
+    "UpdateWindows":  {
+                          "IsPresent":  true
+                      }
+}
+'@
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
+#================================================
+#  [PostOS] AutopilotOOBE Configuration Staging
+#================================================
+Write-Host -ForegroundColor Green "Define Computername:"
+$Serial = Get-WmiObject Win32_bios | Select-Object -ExpandProperty SerialNumber
+$TargetComputername = $Serial.Substring(4,3)
+
+$AssignedComputerName = "AkosCloud-$TargetComputername"
+Write-Host -ForegroundColor Red $AssignedComputerName
+Write-Host ""
+
+Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
+$AutopilotOOBEJson = @"
+{
+    "AssignedComputerName" : "$AssignedComputerName",
+    "AddToGroup":  "AADGroupX",
     "Assign":  {
                    "IsPresent":  true
                },
-    "GroupTag":  "Enterprise",
-    "GroupTagOptions":  [
-                            "Development",
-                            "Enterprise"
-                        ],
+    "GroupTag":  "GroupTagXXX",
     "Hidden":  [
                    "AddToGroup",
-                   "AssignedComputerName",
                    "AssignedUser",
-                   "PostAction"
+                   "PostAction",
+                   "GroupTag",
+                   "Assign"
                ],
     "PostAction":  "Quit",
     "Run":  "NetworkingWireless",
-    "Docs":  "https://autopilotoobe.osdeploy.com/",
-    "Title":  "OSDeploy Autopilot Registration"
+    "Docs":  "https://google.com/",
+    "Title":  "Autopilot Manual Register"
 }
+"@
+
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
+
+#================================================
+#  [PostOS] SetupComplete CMD Command Line
+#================================================
+Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
+$SetupCompleteCMD = @'
+powershell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
+powershell.exe -Command "& {IEX (IRM https://raw.githubusercontent.com/mainsails/OSD/main/OSDCloud-OOBE.ps1)}"
 '@
+$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
-pause
-
-If (-not(Test-Path -Path 'C:\ProgramData\OSDeploy')) {
-    New-Item -ItemType Directory -Path 'C:\ProgramData\OSDeploy' -Force
-}
-$AutopilotOOBEJson | Out-File -FilePath 'C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.JSON' -Encoding ascii -Force
-
-pause
+#=======================================================================
+#   Restart-Computer
+#=======================================================================
+Write-Host  -ForegroundColor Green "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
