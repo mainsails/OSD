3de6044d3e0faeb6b619d95ec27d7086e9a5df91
Sam <mainsails@users.noreply.github.com>
2023-10-02 12:15:50 +0100

    Update and rename Pre-provisioning.ps1 to Pre-provision.ps1
    Name change
    Add required computer restart

--- OSDPad/Pre-provision.ps1
+++ OSDPad/Pre-provision.ps1
@@ -7,3 +7,8 @@ $Params = @{
     UpdateWindows = $true
 }
 Start-OOBEDeploy @Params
+
+# Restart Computer in 10 seconds
+Write-Host  -ForegroundColor Green "Restarting computer in 10 seconds"
+Start-Sleep -Seconds 10
+Restart-Computer
