d6a6346adb618d2ec3b2b15624f69568384189cd
Sam <mainsails@users.noreply.github.com>
2023-10-02 12:57:35 +0100

    Update and rename OSDCloud-WinPE.ps1 to PROD-OSD-Script-WinPE-ZTI-Drivers.ps1
    Update name
    Update process

--- PROD-OSD-Script-WinPE-ZTI-Drivers.ps1
+++ PROD-OSD-Script-WinPE-ZTI-Drivers.ps1
@@ -11,32 +11,23 @@ $Params = @{
 }
 Start-OSDCloud @Params
 
-# Create C:\Windows\Setup\Scripts\SetupComplete.cmd (runs in OOBE)
+# Create C:\Windows\Setup\Scripts\SetupComplete.cmd (automatically runs in OOBE)
 Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
 $SetupCompleteCMD = @'
 PowerShell.exe -Command Set-ExecutionPolicy RemoteSigned -Force
-PowerShell.exe -Command "& { Invoke-Expression -Command (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/mainsails/OSD/main/OSDCloud-OOBE.ps1') }"
+PowerShell.exe -Command "& { Invoke-Expression -Command (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/mainsails/OSD/main/PROD-OSD-Script-OOBE-SetupComplete.ps1') }"
 '@
 $SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
 
-#####
-
-$SetCommand = @'
-@echo off
-PowerShell.exe -Command "& { Invoke-Expression -Command (Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/mainsails/OSD/main/OSDPad/Autopilot.ps1') }"
-'@
-$SetCommand | Out-File -FilePath "C:\Windows\System32\Autopilot.cmd" -Encoding ascii -Force
-
+# Create C:\Windows\System32\Tech.cmd (optional manual run with ctrl-f10 - hosted OOBE helper functions)
+Write-Host -ForegroundColor Green "Create C:\Windows\System32\Tech.cmd"
 $SetCommand = @'
 @echo off
 PowerShell.exe -Command "& { Start-OSDPad -RepoOwner mainsails -RepoName OSD -RepoFolder OSDPad }"
 '@
-$SetCommand | Out-File -FilePath "C:\Windows\System32\Tech.cmd" -Encoding ascii -Force
-
-######
-
+$SetCommand | Out-File -FilePath 'C:\Windows\System32\Tech.cmd' -Encoding ascii -Force
 
 # Restart Computer in 10 seconds
-Write-Host  -ForegroundColor Green "Restarting computer in 10 seconds"
+Write-Host  -ForegroundColor Green "Restart computer in 10 seconds"
 Start-Sleep -Seconds 10
 wpeutil reboot
