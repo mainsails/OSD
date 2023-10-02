1f232d472ee5990248867d5f0c7d07b72f18cfa2
Sam <mainsails@users.noreply.github.com>
2023-10-02 12:50:32 +0100

    Update and rename OSDCloud-Setup-Template.ps1 to PROD-OSD-Setup-Template.ps1
    Update name
    Update template for ADK 2023-09

--- PROD-OSD-Setup-Template.ps1
+++ PROD-OSD-Setup-Template.ps1
@@ -1,11 +1,10 @@
 #Build OSDCloud Template
 
 # Define variables
-$TemplateName     = 'WinPE-kb5029263'
-$CumulativeUpdate = 'C:\temp\windows11.0-kb5029263-x64_4f5fe19bbec786f5e445d3e71bcdf234fe2cbbec.msu'
+$TemplateName = 'WinPE-Win11-22H2-2309'
 
 # Create OSDCloud Template
-New-OSDCloudTemplate -Name $TemplateName -CumulativeUpdate $CumulativeUpdate
+New-OSDCloudTemplate -Name $TemplateName
 
 # Clean up Languages
 $Directories = @('boot','efi','en-us','sources','fonts','resources')
